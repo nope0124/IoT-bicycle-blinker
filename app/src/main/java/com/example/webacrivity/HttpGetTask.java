7978da6458109035a4c07a8dd80e9a315d47c3e8
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-06-21 14:38:10 +0900

    リファクタリング

--- app/src/main/java/com/example/webacrivity/HttpGetTask.java
+++ app/src/main/java/com/example/webacrivity/HttpGetTask.java
@@ -1,6 +1,5 @@
 package com.example.webacrivity;
 
-import android.location.LocationManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.os.AsyncTask;
@@ -27,94 +26,142 @@ public class HttpGetTask extends AsyncTask <String, Void, String> {
     private double mDestinationLongitude = 0.0;
     private ProgressDialog mDialog = null;
 
-    private static JSONArray sResult = new JSONArray();
+    private final String GEOCODING_API = "https://maps.googleapis.com/maps/api/geocode";
+    private final String DIRECTIONS_API = "https://maps.googleapis.com/maps/api/directions";
 
+    private static JSONArray sResult = new JSONArray();
     public static JSONArray getResult() {
         return sResult;
     }
 
 
-    private String API_KEY = "";
+    private final String API_KEY = "";
     public HttpGetTask(Activity parentActivity, TextView routesTextView, EditText destinationEditText, TextView destinationLatitudeTextView, TextView destinationLongitudeTextView, double originLatitude, double originLongitude) {
+        // Activity
         this.mParentActivity = parentActivity;
+
+        // TextView
         this.mRoutesTextView = routesTextView;
-        this.mDestinationEditText = destinationEditText;
         this.mDestinationLatitudeTextView = destinationLatitudeTextView;
         this.mDestinationLongitudeTextView = destinationLongitudeTextView;
+
+        // EditText
+        this.mDestinationEditText = destinationEditText;
+
+        // 現在地情報
         this.mOriginLatitude = originLatitude;
         this.mOriginLongitude = originLongitude;
-        System.out.println("origin_latitude");
-        System.out.println(this.mOriginLatitude);
-        System.out.println("origin_longitude");
-        System.out.println(this.mOriginLongitude);
     }
+
     //タスク開始時
     @Override
     protected void onPreExecute() {
-        mDialog = new ProgressDialog(this.mParentActivity);
-        mDialog.setMessage("");
-        mDialog.show();
+//        mDialog = new ProgressDialog(this.mParentActivity);
+//        mDialog.setMessage("");
+//        mDialog.show();
     }
+
     //メイン処理
     @Override
     protected String doInBackground(String... params) {
         String param = params[0];
         if(param == "Routes") {
             // 目的地の緯度経度を取得
-            double[] destinationLatitudeLongitude = getLatitudeLongitude(this.mDestinationEditText.getText().toString());
+            double[] destinationLatitudeLongitude = getLatitudeLongitudeFromGeocodingAPI(this.mDestinationEditText.getText().toString());
             this.mDestinationLatitude = destinationLatitudeLongitude[0];
             this.mDestinationLongitude = destinationLatitudeLongitude[1];
 
-            System.out.println("destination_latitude");
-            System.out.println(this.mDestinationLatitude);
-            System.out.println("destination_longitude");
-            System.out.println(this.mDestinationLongitude);
+            JSONArray data = getRoutesFromDirectionsAPI(this.mDestinationLatitude, this.mDestinationLongitude);
 
-            JSONArray res = getRoutes(this.mDestinationLatitude, this.mDestinationLongitude);
-            System.out.println("######");
-            System.out.println(res);
-            System.out.println("######");
-
-            sResult = res;
+            sResult = data;
         }else if(param == "RaspberryPi") {
-            HttpURLConnection http = null;
-            BufferedReader reader = null;
-            InputStream in = null;
             String lr = params[1];
             String length = params[2];
             String uri = String.format("http://172.20.10.8/~pi/navi2.php?lr=%s&length=%s", lr, length);
-            System.out.println(uri);
+
+            HttpURLConnection http = null;
+            InputStream in = null;
+            String src = new String();
+
             try {
                 URL url = new URL(uri);
                 http = (HttpURLConnection) url.openConnection();
                 http.setRequestMethod("GET");
                 http.connect();
+                in = http.getInputStream();
+                byte[] line = new byte[1024];
+                int size;
+                while (true) {
+                    size = in.read(line);
+                    if (size <= 0) break;
+                    src += new String(line);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                    }
-                } catch (Exception ignored) {
+                    if (http != null) http.disconnect();
+                    if (in != null) in.close();
+                } catch (Exception e) {
                 }
             }
-
         }
-
         return "";
     }
+
     //タスク終了時
     @Override
     protected void onPostExecute (String string) {
-        mDialog.dismiss();
+//        mDialog.dismiss();
         this.mDestinationLatitudeTextView.setText("目的地緯度: " + Double.toString(this.mDestinationLatitude));
         this.mDestinationLongitudeTextView.setText("目的地経度: " + Double.toString(this.mDestinationLongitude));
-//        this.mRoutesTextView.setText(string);
+    }
 
+
+    private JSONArray getRoutesFromDirectionsAPI(double destinationLatitude, double destinationLongitude) {
+        String uri = String.format("%s/json?language=ja&origin=%.7f,%.7f&destination=%.7f,%.7f&key=%s", DIRECTIONS_API, mOriginLatitude, mOriginLongitude, destinationLatitude, destinationLongitude, API_KEY);
+
+        JSONArray src = new JSONArray();
+
+        JSONObject data = getDataFromUri(uri);
+        try {
+            // 経路のステップを取得
+            JSONArray routes = data.getJSONArray("routes");
+            JSONObject route = routes.getJSONObject(0);
+            JSONArray legs = route.getJSONArray("legs");
+            JSONObject leg = legs.getJSONObject(0);
+            JSONArray steps = leg.getJSONArray("steps");
+
+            src = steps;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return src;
+    }
+
+    public double[] getLatitudeLongitudeFromGeocodingAPI(String destination) {
+        String uri = String.format("%s/json?address=%s&key=%s", GEOCODING_API, destination, API_KEY);
+
+        // 初期化
+        double latitude = 0.0;
+        double longitude = 0.0;
+
+        JSONObject data = getDataFromUri(uri);
+
+        try {
+            // 経路のステップを取得
+            JSONArray results = data.getJSONArray("results");
+            JSONObject result = results.getJSONObject(0);
+            JSONObject geometry = result.getJSONObject("geometry");
+            JSONObject location = geometry.getJSONObject("location");
+
+            // 緯度経度を取得
+            latitude  = location.getDouble("lat");
+            longitude = location.getDouble("lng");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new double[] { latitude, longitude };
     }
 
     public JSONObject getDataFromUri(String uri) {
@@ -164,62 +211,6 @@ public class HttpGetTask extends AsyncTask <String, Void, String> {
             }
         }
         return data;
-    }
-
-
-    private JSONArray getRoutes(double destination_latitude, double destination_longitude) {
-        String uri = String.format("https://maps.googleapis.com/maps/api/directions/json?language=ja&origin=%.7f,%.7f&destination=%.7f,%.7f&key=%s", mOriginLatitude, mOriginLongitude, destination_latitude, destination_longitude, API_KEY);
-        JSONArray src = new JSONArray();;
-
-        JSONObject data = getDataFromUri(uri);
-        try {
-            // 経路のステップを取得
-            JSONArray routes = data.getJSONArray("routes");
-            JSONObject route = routes.getJSONObject(0);
-            JSONArray legs = route.getJSONArray("legs");
-            JSONObject leg = legs.getJSONObject(0);
-            JSONArray steps = leg.getJSONArray("steps");
-
-            src = steps;
-
-            // 各ステップの指示を表示
-//            for (int i = 0; i < steps.length(); i++) {
-//                JSONObject step = steps.getJSONObject(i);
-//                String instruction = step.getString("html_instructions");
-//                System.out.println(instruction);
-//                src += new String(instruction);
-//                src += new String("\n");
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return src;
-    }
-
-    public double[] getLatitudeLongitude(String destination) {
-
-        String uri = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s", destination, API_KEY);
-
-        double latitude = 0.0;
-        double longitude = 0.0;
-
-        JSONObject data = getDataFromUri(uri);
-
-        try {
-            // 経路のステップを取得
-            JSONArray results = data.getJSONArray("results");
-            JSONObject result = results.getJSONObject(0);
-            JSONObject geometry = result.getJSONObject("geometry");
-            JSONObject location = geometry.getJSONObject("location");
-            latitude  = location.getDouble("lat");
-            longitude = location.getDouble("lng");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String str = Double.toString(latitude) + Double.toString(longitude);
-//        return str;
-        return new double[] { latitude, longitude };
     }
 }
 
--- app/src/main/java/com/example/webacrivity/MainActivity.java
+++ app/src/main/java/com/example/webacrivity/MainActivity.java
@@ -16,12 +16,7 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.util.Log;
 import android.view.View;
-import com.example.webacrivity.HttpGetTask;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.regex.Matcher;
@@ -29,10 +24,6 @@ import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//    private TextView mReturnTextView;
-//    private EditText mEditTextName;
-//
-//    private TextView locationText;
     private LocationManager locationManager;
 
     // Button
@@ -49,21 +40,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView mDestinationLongitudeTextView;
 
     // MainActivity
-    private MainActivity activity = this;
+    private MainActivity mActivity = this;
 
     // JSONArray
-    private JSONArray steps = new JSONArray();
+    private JSONArray mSteps = new JSONArray();
 
-    // idx
-    private int idx = 0;
+    // int
+    private int mRouteIndex = 0;
+    private int mLocationChangedCount = 0;
+    private final int mPerCount = 6; // 1分に10回実行
 
-    private int cnt = 0;
-
-    private boolean startFlag = true;
+    private boolean mStartFlag = true;
 
     private static final int PERMISSIONS_REQUEST = 1;
 
-    private TextView mDestinationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,31 +82,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         Log.d("WebActivity", "wlog onClick()");
         getLocation();
-//        if(startFlag) {
-//            System.out.println(100);
-//            HttpGetTask task = new HttpGetTask(activity, mRoutesTextView, mDestinationEditText, mDestinationLatitudeTextView, mDestinationLongitudeTextView, 36.1111765, 140.1004090);
-//            task.execute("Routes", "", "");
-//            try {
-//                Thread.sleep(5000); // 5秒待つ
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            steps = HttpGetTask.getResult();
-//            try {
-//                JSONObject step = steps.getJSONObject(idx);
-//                System.out.println(step);
-//                String instruction = step.getString("html_instructions");
-//                System.out.println(instruction);
-//                JSONObject end_location = step.getJSONObject("end_location");
-//                System.out.println(end_location);
-//                double currentDestinationLatitude = end_location.getDouble("lat");
-//                double currentDestinationLongitude = end_location.getDouble("lng");
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println(steps);
-//            startFlag = false;
-//        }
     }
 
     private void getLocation() {
@@ -130,53 +95,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private final LocationListener locationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
+            mLocationChangedCount++;
             mOriginLatitudeTextView.setText("現在地緯度: " + location.getLatitude());
             mOriginLongitudeTextView.setText("現在地経度: " + location.getLongitude());
-            if(startFlag) {
-                System.out.println(100);
-                HttpGetTask task = new HttpGetTask(activity, mRoutesTextView, mDestinationEditText, mDestinationLatitudeTextView, mDestinationLongitudeTextView, location.getLatitude(), location.getLongitude());
+
+            System.out.println(mLocationChangedCount);
+            // １回目だけ、Google Directions APIを叩く
+            if(mStartFlag == true) {
+                mStartFlag = false;
+
+                // Google Directions APIを叩き、目的地の緯度経度、までの経路情報を取得
+                HttpGetTask task = new HttpGetTask(mActivity, mRoutesTextView, mDestinationEditText, mDestinationLatitudeTextView, mDestinationLongitudeTextView, location.getLatitude(), location.getLongitude());
                 task.execute("Routes", "", "");
+
+                // 直後のHttpGetTask.getResult()が失敗するため、5秒待つ
                 try {
                     Thread.sleep(5000); // 5秒待つ
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                steps = HttpGetTask.getResult();
-                startFlag = false;
-            }else {
-                if(cnt % 5 != 0) return;
-                steps = HttpGetTask.getResult();
-                System.out.println(200);
-                System.out.println(steps);
+
+                // 経路情報を取得
+                mSteps = HttpGetTask.getResult();
+            }else if(mStartFlag == false && mLocationChangedCount % mPerCount == 0) {
+
                 try {
-                    JSONObject step = steps.getJSONObject(idx);
-                    System.out.println(300);
+                    JSONObject step = mSteps.getJSONObject(mRouteIndex);
+
+                    // 経路説明
                     String instruction = step.getString("html_instructions");
-                    System.out.println(400);
+
+                    // 一時終点までの緯度経度
                     JSONObject end_location = step.getJSONObject("end_location");
-                    System.out.println(500);
                     double currentDestinationLatitude = end_location.getDouble("lat");
                     double currentDestinationLongitude = end_location.getDouble("lng");
+
+                    // 一時終点までの距離を求めるために一旦定義
                     Location currentDestinationLocation = new Location("dummyprovider");
                     currentDestinationLocation.setLatitude(currentDestinationLatitude);
                     currentDestinationLocation.setLongitude(currentDestinationLongitude);
+
+                    // 一時終点までの距離
                     double distanceToCurrentDestination = location.distanceTo(currentDestinationLocation);
-                    Integer lr = convertAndCheckDirection(instruction);
+
+                    // このタイミングで経路説明と、一時終点までの距離を描画
                     mRoutesTextView.setText(String.format("%s\n%s", instruction, Double.toString(distanceToCurrentDestination)));
-                    HttpGetTask task = new HttpGetTask(activity, mRoutesTextView, mDestinationEditText, mDestinationLatitudeTextView, mDestinationLongitudeTextView, location.getLatitude(), location.getLongitude());
-                    task.execute("RaspberryPi", Integer.toString(lr), Double.toString(distanceToCurrentDestination));
+
+                    // RaspberryPiに渡す引数
+                    int lr = convertAndCheckDirection(instruction);
+                    int length = (int)distanceToCurrentDestination;
+
+                    // RaspberryPiを呼び出す
+                    HttpGetTask task = new HttpGetTask(mActivity, mRoutesTextView, mDestinationEditText, mDestinationLatitudeTextView, mDestinationLongitudeTextView, location.getLatitude(), location.getLongitude());
+                    task.execute("RaspberryPi", Integer.toString(lr), Integer.toString(length));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-
-//            this.mRoutesTextView.setText(HttpGetTask.getResult());
-
-//
         }
 
-
-        public Integer convertAndCheckDirection(String unicodeEscapedString) {
+        public int convertAndCheckDirection(String unicodeEscapedString) {
             // UTF-8文字列に変換
             String utf8String = unescapeUnicode(unicodeEscapedString);
 
@@ -184,16 +162,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String text = removeHtmlTags(utf8String);
 
             // "右"が含まれる場合は1を返す
-            if (text.contains("右")) {
-                return 1;
-            }
+            if (text.contains("右")) return 1;
 
             // "左"が含まれる場合は0を返す
-            if (text.contains("左")) {
-                return 0;
-            }
+            if (text.contains("左")) return 0;
 
-            // "右"も"左"も含まれない場合はnullを返す
+            // "右"も"左"も含まれない場合は1を返す
             return 1;
         }
 
@@ -218,7 +192,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return text;
         }
 
-//
         public void onStatusChanged(String provider, int status, Bundle extras) { }
 
         public void onProviderEnabled(String provider) { }
